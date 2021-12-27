afc7ffd05cfa10536875e295f69d916fda4d6e56
aiman-arainx <84847654+aiman-arainx@users.noreply.github.com>
2021-12-27 12:01:52 +0500

    database order fulfillment tale migration

---
+++ database/migrations/2021_12_24_121016_create_orderfulfillment_payment_logs.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderfulfillmentPaymentLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orderfulfillment_payment_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->index('order_id')->constrained('orders');
+            $table->decimal('paid_amount',10,2)->nullable();
+            $table->unsignedInteger('added_by')->nullable()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orderfulfillment_payment_logs');
+    }
+}
